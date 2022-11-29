cbbf6be64c35a610832b3f7a4570b0806ce7d8a3
Hampus Fredriksson <42962227+hampusfredriksson@users.noreply.github.com>
2022-11-29 17:12:44 +0100

    update index, event pages

--- pages/event.tsx
+++ pages/event.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { NextPage } from "next";
+import Link from "next/link";
 
-const event = () => {
-  return <div className="text-orange-400">event</div>;
+const Event: NextPage = ({ posts }) => {
+  return (
+    <div>
+      <div className="flex justify-center mt-24">
+        <h1 className="font-mono text-4xl">Events</h1>
+      </div>
+
+      {posts.data.map((post) => (
+        <Link href={`/event/${post.attributes.slug}`} key={post.id}>
+          <a>
+            <h2>{post.attributes.Title}</h2>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
-export default event;
+export async function getStaticProps() {
+  // get posts from api
+
+  const res = await fetch(`http://localhost:1337/api/posts`);
+
+  const posts = await res.json();
+
+  return {
+    props: { posts },
+  };
+}
+
+export default Event;
